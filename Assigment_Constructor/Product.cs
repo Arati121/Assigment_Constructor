f59f1aab32f238fbc084f3a318327b5d9ceb09f7
Arati121 <avbhosale@mitaoe.ac.in>
2022-03-23 16:22:29 +0530

    Add project files.

---
+++ Assigment_Constructor/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assigment_Constructor
+{
+    public class Product
+    {
+        private int product_code;
+        private string product_Name;
+        private int product_price;
+         
+        public Product()
+        {
+            product_code = 101;
+            product_Name = "Parle";
+            product_price = 15000;
+        }
+
+        public Product(int product_code,string product_name,int product_price)
+        {
+            this.product_code = product_code;
+            this.product_Name = product_name;
+            this.product_price = product_price;
+        }
+
+        public string Tostring()
+        {
+            return "product code= " + product_code + "Product name= " + product_Name + " product price= " + product_price;
+        }
+
+    }
+}
---
+++ Assigment_Constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Assigment_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product();
+            p1.Tostring();
+
+            Product p2 = new Product(10,"maggi",20000);
+            p2.Tostring();
+
+
+        }
+    }
+}
